21febdbf0365b02ad5a8faea7e2f8786d1b761da
Brian Canzanella <brian@codestream.com>
2021-01-05 16:21:15 -0500

    working on files

--- shared/agent/src/managers/scmManager.ts
+++ shared/agent/src/managers/scmManager.ts
@@ -1375,15 +1375,12 @@ export class ScmManager {
 
 		const remoteBranch = await git.getBranchRemote(repo.path, request.branchName);
 		if (!remoteBranch) {
-			throw new Error(`fetchRemoteBranch: Couldn't find branchRemote for ${repo.path} and ${request.branchName}`);
+			throw new Error(
+				`fetchRemoteBranch: Couldn't find branchRemote for ${repo.path} and ${request.branchName}`
+			);
 		}
 
-		const { error } = await git.fetchRemoteBranch(
-			repo.path,
-			".",
-			remoteBranch,
-			request.branchName
-		);
+		const { error } = await git.fetchRemoteBranch(repo.path, ".", remoteBranch, request.branchName);
 		if (error) {
 			throw new Error(error);
 		}
@@ -1410,7 +1407,11 @@ export class ScmManager {
 		await git.fetchAllRemotes(repoPath);
 
 		const baseBranchRemote = await git.getBranchRemote(repoPath, request.branchName);
-		const commitsBehindOrigin = await git.getBranchCommitsStatus(repoPath, baseBranchRemote!, request.branchName);
+		const commitsBehindOrigin = await git.getBranchCommitsStatus(
+			repoPath,
+			baseBranchRemote!,
+			request.branchName
+		);
 
 		return {
 			commitsBehindOrigin
@@ -1491,6 +1492,22 @@ export class ScmManager {
 				}
 			};
 		}
+		if (!request.baseSha)
+			return {
+				sha: "",
+				error: {
+					message: "baseSha is required",
+					type: "REPO_NOT_FOUND"
+				}
+			};
+		if (!request.headSha)
+			return {
+				sha: "",
+				error: {
+					message: "headSha is required",
+					type: "REPO_NOT_FOUND"
+				}
+			};
 		try {
 			const shas = [request.baseSha, request.headSha];
 			const results = await Promise.all(
